bf09db0ad7820c4553f2e631682e96e5d21c0f61
jaroslav.filo <jaroslav.filo@k2ol.cz>
2025-12-30 12:18:14 +0100

    add docking of the info panel

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,20 @@ import { Box } from "@chakra-ui/react";
 import "./App.css";
 import { Map } from "./components/Map";
 import { Header } from "./components/Header";
+import { Dock } from "react-dock";
 
 function App() {
     return (
         <Box as="section" height="100vh">
             <Header />
             <Map />
+            <Dock
+                position="right"
+                isVisible
+                dockStyle={{ backgroundColor: "black" }}
+            >
+                Content of the dock
+            </Dock>
         </Box>
     );
 }
