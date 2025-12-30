adac8eb500e7d5a13323e9467e58761e5ac869bd
jaroslav.filo <jaroslav.filo@k2ol.cz>
2025-12-30 15:55:26 +0100

    add multiple markers based on data

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,10 +1,12 @@
 import "leaflet/dist/leaflet.css";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import fakeData from "../consts/fakeData.json";
+import { icons } from "../consts/icons";
 
 export const Map = () => {
     return (
         <MapContainer
-            center={[51.505, -0.09]}
+            center={[50, 50]}
             zoom={13}
             scrollWheelZoom={false}
             style={{ height: "95vh", width: "100%" }}
@@ -13,11 +15,17 @@ export const Map = () => {
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={[51.505, -0.09]}>
-                <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-            </Marker>
+            {fakeData.map((data) => (
+                <Marker
+                    key={data.id}
+                    position={data.position as [number, number]}
+                    icon={icons[data.team as keyof typeof icons]}
+                >
+                    <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                </Marker>
+            ))}
         </MapContainer>
     );
 };
