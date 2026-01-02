9af0c61922ec484dad7f89b50e5d8a992e97ab8b
Jaro <jaroslavfilo95@gmail.com>
2026-01-02 09:33:10 +0100

    add alignment icons

--- src/components/DockingControls.tsx
+++ src/components/DockingControls.tsx
@@ -1,43 +1,49 @@
 import { Box, Button } from "@chakra-ui/react";
 import { useDockContext } from "../provider/DockProvider";
+import {
+  LuAlignEndHorizontal,
+  LuAlignEndVertical,
+  LuAlignStartHorizontal,
+  LuAlignStartVertical,
+} from "react-icons/lu";
 
 export const DockingControls = () => {
-    const { handlePosition } = useDockContext();
+  const { handlePosition } = useDockContext();
 
-    return (
-        <Box as="div">
-            <Button
-                variant="outline"
-                size="sm"
-                mb="10px"
-                onClick={() => handlePosition("left")}
-            >
-                Left
-            </Button>
-            <Button
-                variant="outline"
-                size="sm"
-                mb="10px"
-                onClick={() => handlePosition("right")}
-            >
-                Right
-            </Button>
-            <Button
-                variant="outline"
-                size="sm"
-                mb="10px"
-                onClick={() => handlePosition("top")}
-            >
-                Top
-            </Button>
-            <Button
-                variant="outline"
-                size="sm"
-                mb="10px"
-                onClick={() => handlePosition("bottom")}
-            >
-                Bottom
-            </Button>
-        </Box>
-    );
+  return (
+    <Box as="div">
+      <Button
+        variant="outline"
+        size="sm"
+        mb="10px"
+        onClick={() => handlePosition("left")}
+      >
+        <LuAlignStartVertical />
+      </Button>
+      <Button
+        variant="outline"
+        size="sm"
+        mb="10px"
+        onClick={() => handlePosition("right")}
+      >
+        <LuAlignEndVertical />
+      </Button>
+      <Button
+        variant="outline"
+        size="sm"
+        mb="10px"
+        onClick={() => handlePosition("top")}
+      >
+        <LuAlignStartHorizontal />
+      </Button>
+      <Button
+        variant="outline"
+        size="sm"
+        mb="10px"
+        onClick={() => handlePosition("bottom")}
+      >
+        <LuAlignEndHorizontal />
+      </Button>
+    </Box>
+  );
 };
