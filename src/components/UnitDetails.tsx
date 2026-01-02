8d5418fb92a688d52e97095fe3f6f5adb033905a
Jaro <jaroslavfilo95@gmail.com>
2026-01-02 09:33:35 +0100

    add destination display

--- src/components/UnitDetails.tsx
+++ src/components/UnitDetails.tsx
@@ -2,24 +2,27 @@ import { Box, Text } from "@chakra-ui/react";
 import { useSocketContext } from "../provider/SocketProvider";
 
 export const UnitDetails = () => {
-    const { units, selectedUnit } = useSocketContext();
+  const { units, selectedUnit } = useSocketContext();
 
-    const unitToDisplay = units.find((unit) => unit.id === selectedUnit);
+  const unitToDisplay = units.find((unit) => unit.id === selectedUnit);
 
-    if (!unitToDisplay) return <Text>Select a unit to see details.</Text>;
+  if (!unitToDisplay) return <Text>Select a unit to see details.</Text>;
 
-    return (
-        <Box as="div" padding="10px">
-            <Text as="h3">Unit Details Component</Text>
-            <Text>Type: {unitToDisplay.type}</Text>
-            <Text>Callsign: {unitToDisplay.callsign}</Text>
-            <Text>
-                Position (lat, lng): {unitToDisplay.position[0]},{" "}
-                {unitToDisplay.position[1]}
-            </Text>
-            <Text>Current task: {unitToDisplay.task}</Text>
-            <Text>Current speed: {unitToDisplay.speed}</Text>
-            <Text>Team: {unitToDisplay.team}</Text>
-        </Box>
-    );
+  return (
+    <Box as="div" padding="10px">
+      <Text as="h3">Unit Details Component</Text>
+      <Text>Type: {unitToDisplay.type}</Text>
+      <Text>Callsign: {unitToDisplay.callsign}</Text>
+      <Text>
+        Position (lat, lng): {unitToDisplay.positionLat},{" "}
+        {unitToDisplay.positionLng}
+      </Text>
+      <Text>
+        Destination (lat, lng): {unitToDisplay.destLat}, {unitToDisplay.destLng}
+      </Text>
+      <Text>Current task: {unitToDisplay.task}</Text>
+      <Text>Current speed: {unitToDisplay.speed}</Text>
+      <Text>Team: {unitToDisplay.team}</Text>
+    </Box>
+  );
 };
